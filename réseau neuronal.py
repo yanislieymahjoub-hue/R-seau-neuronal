f313f813713c022bbe467ab13167f00a3bf2d611
yanislieymahjoub-hue <yanis.lieymahjoub@gmail.com>
2026-06-08 15:43:18 +0200

    Ajout d'une couche de neurones (plusieurs neurones en parallèle)

--- réseau neuronal.py
+++ réseau neuronal.py
@@ -6,3 +6,21 @@ def réseau_neuronal(entrées,poids,biais):
 
 A = réseau_neuronal ([1.0,2.0],[8.0,9.0],7.0)
 print(A)
+
+
+def couche(entrées,neurone):
+    res = []
+    for i,j in neurone :
+        G = réseau_neuronal (entrées,i,j)
+        res.append(G)
+    return res
+
+
+neurones = [
+    ([8.0, 9.0], 7.0),
+    ([1.0, -2.0], 0.5),
+    ([-3.0, 4.0], -1.0),]
+
+
+B = couche ([0.0,1.0],neurones)
+print (B)
